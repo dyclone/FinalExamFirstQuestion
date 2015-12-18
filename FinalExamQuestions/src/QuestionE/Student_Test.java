cc8d06e37bd28e68dbbfbfa71df7c197f45c56a6
dcyclone <dcortes@udel.edu>
2015-12-18 17:04:47 -0500

    Final

--- FinalExamQuestions/src/QuestionE/Student_Test.java
+++ FinalExamQuestions/src/QuestionE/Student_Test.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -40,9 +44,30 @@ public class Student_Test {
 			e.printStackTrace();
 		}
 		Student stu1 = new Student("Bert", "Randall", "Gibbons", dBirthDate, "214 Labrador Lane", "302-893-6889", "bgibbons@udel.edu","MIS");
+		Student stu2 = new Student("Dan", "Randall", "Cortes", dBirthDate, "114 Labrador Lane", "402-893-6889", "vgibbons@udel.edu","GIS");
+		Student stu3 = new Student("Jeff", "Randall", "Nelson", dBirthDate, "314 Labrador Lane", "502-893-6889", "ggibbons@udel.edu","PIS");
+		Student stu4 = new Student("Steve", "Randall", "Coleman", dBirthDate, "414 Labrador Lane", "602-893-6889", "hgibbons@udel.edu","OIS");
+		Student stu5 = new Student("Bob", "Randall", "Dude", dBirthDate, "514 Labrador Lane", "702-893-6889", "kgibbons@udel.edu","DIS");
+		
+		HashMap hm = new HashMap();
+		 hm.put("MIS", stu1);
+		 hm.put("GIS", stu2);
+		 hm.put("PIS", stu3);
+		 hm.put("OIS", stu4);
+		 hm.put("DIS", stu5);
+		 
+		// Get a set of the entries
+	      Set set = hm.entrySet();
+	      // Get an iterator
+	      Iterator i = set.iterator();
+	      // Display elements
+	      while(i.hasNext()) {
+	         Map.Entry me = (Map.Entry)i.next();
+	         System.out.print(me.getKey() + ": ");
+	         System.out.println(me.getValue());
 
-
-		assertTrue(1==1);
+		assertTrue(5==5);
 	}
 
+	}
 }
